3ef19ab1cb9e3a9a77a0b384422e8c128a751756
LiLiLiLaLa <192130809@qq.com>
2018-08-31 23:26:31 +0800

    队列和栈基本接口

---
+++ test.c
@@ -0,0 +1,32 @@
+#include "Stack.h"
+#include "Queue.h"
+
+int main()
+{
+	//DataType tmp = 0;
+	//Stack s;
+	//StackInit(&s); 
+	//StackPush(&s, 1); 
+	//StackPush(&s, 2);
+	//StackPush(&s, 3); 
+	//StackPush(&s, 5);
+	//StackPush(&s, 3);
+	////StackDestory(&s);
+	////tmp = StackTop(&s);
+	////printf("栈顶的元素是：%d", tmp);
+	//if (!StackEmpty(&s))
+	//{
+	//	printf("栈为空\n");
+	//}
+	//else
+	//{
+	//	printf("栈非空\n");
+	//}
+	//tmp = StackSize(&s);
+	//printf("栈中元素个数：%d", tmp);
+	//TestQueue();
+	TestQueueByTwoStack();
+
+	system("pause");
+	return 0;
+}
